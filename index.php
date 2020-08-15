2df68b2606eb25701da4a4112d92e5bfa95e0869
JamalGuyo <jamalguyo08@gmail.com>
2020-08-15 17:07:24 +0300

    style the cards

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php include('header.php') ?>
 <!-- body content -->
-
+<main>
+    <div class="card">
+        <!-- img -->
+        <img src="https://images.unsplash.com/photo-1536304993881-ff6e9eefa2a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="food img"/>
+        <div>
+           <div class="title">
+               <h1>Spaghetti</h1>
+               <h2 class="secondary">$100.00</h2>
+           </div>
+           <div class="content">
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet quas, quo laborum quidem culpa quisquam accusamus iusto iure! Ad unde dolore porro fugiat magni facilis aperiam sit repudiandae dignissimos voluptatem?</p>
+           </div>
+           <button>order</button>
+        </div>
+    </div>
+</main>
 <!-- end of body content -->
 <?php include('footer.php') ?>
